8d6dac8ed7c3f398144374378439e0f61e451999
blakea83 <blake.w.anderson@gmail.com>
2015-02-21 14:34:41 -0600

    Documentation Pages

--- ui.R
+++ ui.R
@@ -1,16 +1,28 @@
-shinyUI(pageWithSidebar(
-        headerPanel("Wastewater Treatment Plant State Point Analysis"),
-        sidebarPanel(
-                numericInput('INF_Q', 'Flow Rate of Water into the Plant in millions gallons per day (mgd)', 0, min = 0, max = 100, step = 0.1),
-                submitButton('Submit'),
-                numericInput('MLSS', 'Solids Concentration into the Clarifier g/L', 0, min = 0, max = 10000, step = 10), 
-                submitButton('Submit'),
-                sliderInput('RAS_Q','Let the RAS rate in mgd',value=1,min=0,max=100,step=0.1,),
-                submitButton('Submit')
-        ),
-        mainPanel(
-          plotOutput('State_Point')
-  
+library(shiny)
+
+shinyUI(navbarPage(
+                title='State Point Analysis',
+                tabPanel('Analysis',
+                         sidebarLayout(
+                         sidebarPanel(
+                                numericInput('INF_Q', 'Flow Rate of Water into the Plant in millions gallons per day (mgd)', 0, min = 0, max = 100, step = 0.1),
+                                submitButton('Submit'),
+                                numericInput('MLSS', 'Solids Concentration into the Clarifier g/L', 0, min = 0, max = 10000, step = 10), 
+                                submitButton('Submit'),
+                                sliderInput('RAS_Q','Let the RAS rate in mgd',value=1,min=0,max=100,step=0.1,),
+                                submitButton('Submit')             
+                                 
+                                 ),
+                         mainPanel(
+                                plotOutput('State_Point')
+                                ),
+                         )
+                        ),
+                tabPanel('Documentation')
+                )
+                        
         )
-))
+
+
+
 
