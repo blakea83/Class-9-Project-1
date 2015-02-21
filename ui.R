795a60d318958d8c3b50631041dcc69ca096c631
blakea83 <blake.w.anderson@gmail.com>
2015-02-21 09:48:56 -0600

    RAS slider bar

--- ui.R
+++ ui.R
@@ -5,9 +5,8 @@ shinyUI(pageWithSidebar(
                 submitButton('Submit'),
                 numericInput('MLSS', 'Solids Concentration into the Clarifier g/L', 0, min = 0, max = 10000, step = 10), 
                 submitButton('Submit'),
-                numericInput('RAS_Q', 'RAS Flow Rates (mgd)', 0, min = 0, max = 100, step = 0.1), 
+                sliderInput('RAS_Q','Let the RAS rate in mgd',value=1,min=0,max=100,step=0.1,),
                 submitButton('Submit')
-                #sliderInput('RAS_Q','Let the RAS rate in mgd',value=1,min=0,max=100,step=0.1,)
         ),
         mainPanel(
           plotOutput('State_Point')
